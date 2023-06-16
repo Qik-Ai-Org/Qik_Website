8d81033d6e75ac4f42ede898c0c9b729f67a5fde
prafullnavkar-nxg <prafull.navkar@nxglabs.com>
2023-06-16 12:57:39 +0530

    replaced hashrouter with browserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Academy from "./components/Academy.js";
 import Navigation from "./components/Navigation.js";
